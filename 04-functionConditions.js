b8d4ae5c8fbc4296ce3765d905d7ba94c7a5ff27
Ian Fiterman <igf9529@nyu.edu>
2024-11-03 16:32:39 -0500

    Fixed 04

--- 04-functionConditions.js
+++ 04-functionConditions.js
@@ -1,5 +1,5 @@
 function guessMyNumber(guess) {
-    let myNum = Math.floor(Math.random() * 11);
+    let myNum = Math.floor(Math.random() * 10) + 1;
     
     if (guess === myNum) {
         console.log("You guessed my number!");
